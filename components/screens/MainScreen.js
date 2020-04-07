a1f6c7990979ad4ad8964f75004c7e383724fba3
micheleantonazzi <micheleantonazzi@gmail.com>
2020-04-07 12:58:41 +0200

    MainScreen.js: added main screen

---
+++ components/screens/MainScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Button, StyleSheet, Text, View} from "react-native";
+
+const MainScreen = props => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default MainScreen;
