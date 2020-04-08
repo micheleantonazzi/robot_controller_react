93440a2986c08bf8035559c0d43c53b455eb51d7
micheleantonazzi <micheleantonazzi@gmail.com>
2020-04-08 13:14:38 +0200

    TextInputStyled: added first styled component

---
+++ components/styledComponets/TextInputStyled.js
@@ -0,0 +1,19 @@
+import React, {useContext} from 'react';
+import {TextInput} from 'react-native';
+import AppThemeContext from '../contexts/AppThemeContext';
+
+const TextInputStyled = props => {
+  const theme = useContext(AppThemeContext);
+
+  return (
+    <TextInput
+      style={{
+        borderColor: theme.colors.border,
+        color: theme.colors.inputText,
+        ...props.style,
+      }}
+    />
+  );
+};
+
+export default TextInputStyled;
