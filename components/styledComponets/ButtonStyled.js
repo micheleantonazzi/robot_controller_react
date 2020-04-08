ad548e27a75c7812396086c0a1ba89f712d99eb3
micheleantonazzi <micheleantonazzi@gmail.com>
2020-04-08 15:20:54 +0200

    ButtonStyled: added button with theme

---
+++ components/styledComponets/ButtonStyled.js
@@ -0,0 +1,17 @@
+import React, {useContext} from 'react';
+import {Button} from 'react-native';
+import AppThemeContext from '../contexts/AppThemeContext';
+
+const ButtonStyled = props => {
+  const theme = useContext(AppThemeContext);
+  return (
+    <Button
+      title={props.title}
+      onPress={props.onPress}
+      style={{...props.style}}
+      color={theme.colors.primary}
+    />
+  );
+};
+
+export default ButtonStyled;
