3541a8b5f7a1fddc5d7a6312097c4aa4ab984444
micheleantonazzi <micheleantonazzi@gmail.com>
2020-04-08 11:46:14 +0200

    AppDarkTheme.js: added theme

---
+++ components/themes/AppThemeDark.js
@@ -0,0 +1,11 @@
+export const AppThemeDark = {
+  dark: false,
+  colors: {
+    primary: '#0eab9a',
+    background: '#101d25',
+    card: '#232d36',
+    text: '#8f969e',
+    border: '#0eab9a',
+    statusBar: '#0f1e25',
+  },
+};
