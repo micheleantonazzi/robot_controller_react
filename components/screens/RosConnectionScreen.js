36a4cac32c0f06be3e70ab0c560b0ce336e5a44b
micheleantonazzi <micheleantonazzi@gmail.com>
2020-04-08 15:21:27 +0200

    RosConnectionScreen.js: added button styled

--- components/screens/RosConnectionScreen.js
+++ components/screens/RosConnectionScreen.js
@@ -1,11 +1,15 @@
-import React, {useContext} from 'react';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
 import TextInputStyled from '../styledComponets/TextInputStyled';
+import ButtonStyled from '../styledComponets/ButtonStyled';
 
 const RosConnectionScreen = props => {
   return (
     <View style={styles.mainViewStyle}>
       <TextInputStyled style={styles.textInputAddressStyle} />
+      <View style={styles.buttonBoxStyle}>
+        <ButtonStyled title={'Connect'} onPress={() => {}} />
+      </View>
     </View>
   );
 };
@@ -13,10 +17,15 @@ const RosConnectionScreen = props => {
 const styles = StyleSheet.create({
   mainViewStyle: {
     flex: 1,
-    padding: 20,
+    padding: 30,
+    justifyContent: 'center',
   },
   textInputAddressStyle: {
     borderBottomWidth: 2,
+    fontSize: 17,
+  },
+  buttonBoxStyle: {
+    margin: 20,
   },
 });
 
