4e44067e93b67ec68dd923ccba7d156b0ed7acd8
micheleantonazzi <micheleantonazzi@gmail.com>
2020-04-10 17:30:26 +0200

    Strings.js: added strings

---
+++ components/definitions/Strings.js
@@ -0,0 +1,5 @@
+export const Strings = {
+  firstConnectionError: 'Unable to connect with master',
+  connectionError: 'You have been disconnected to master',
+  connectionSuccess: 'Connect with master!',
+};
