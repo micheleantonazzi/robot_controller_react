1071e01a3b6eb174402bd3418eface48ded37d1f
micheleantonazzi <micheleantonazzi@gmail.com>
2020-04-11 15:20:11 +0200

    App.js: created first navigation structure

--- App.js
+++ App.js
@@ -2,15 +2,27 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import MainScreen from './components/screens/MainScreen';
-import RosConnectionScreen from './components/screens/RosConnectionScreen';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import {AppThemeDark} from './components/definitions/AppThemeDark';
 import AppThemeContext from './components/contexts/AppThemeContext';
 import RosSettingsContext from './components/contexts/RosSettingsContext';
 import {RosSettings} from './components/definitions/RosSettings';
+import RobotControlScreen from './components/screens/RobotControlScreen';
+import {createStackNavigator} from '@react-navigation/stack';
 
-const Stack = createStackNavigator();
+// Navigators
+const Drawer = createDrawerNavigator();
+const RobotControllerStack = createStackNavigator();
+
+const getRobotControllerStackNavigator = () => (
+  <RobotControllerStack.Navigator>
+    <RobotControllerStack.Screen
+      name={'RobotControllerScreen'}
+      component={RobotControlScreen}
+      options={{title: 'Robot Controller'}}
+    />
+  </RobotControllerStack.Navigator>
+);
 
 const App = () => {
   const theme = AppThemeDark;
@@ -21,10 +33,12 @@ const App = () => {
       <RosSettingsContext.Provider value={rosSettings}>
         <StatusBar backgroundColor={theme.colors.statusBar} />
         <NavigationContainer theme={theme}>
-          <Stack.Navigator>
-            <Stack.Screen name="Connection" component={RosConnectionScreen} />
-            <Stack.Screen name="Home" component={MainScreen} />
-          </Stack.Navigator>
+          <Drawer.Navigator>
+            <Drawer.Screen
+              name={'RobotControlScree'}
+              component={getRobotControllerStackNavigator}
+            />
+          </Drawer.Navigator>
         </NavigationContainer>
       </RosSettingsContext.Provider>
     </AppThemeContext.Provider>
