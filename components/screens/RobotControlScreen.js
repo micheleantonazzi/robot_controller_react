2a709d1800ececb0abc61c7504a6f2dc8ed888da
micheleantonazzi <micheleantonazzi@gmail.com>
2020-04-11 15:19:51 +0200

    RobotControlScreen.js: renamed

---
+++ components/screens/RobotControlScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const RobotControlScreen = props => {
+  return (
+    <View>
+      <Text>ciao</Text>
+    </View>
+  );
+};
+
+export default RobotControlScreen;
