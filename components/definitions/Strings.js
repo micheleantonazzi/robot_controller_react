b74d20529d444fcb894ffae954701a7dcf854296
micheleantonazzi <micheleantonazzi@gmail.com>
2020-04-11 16:37:43 +0200

    Strings.js: added screen names

--- components/definitions/Strings.js
+++ components/definitions/Strings.js
@@ -2,4 +2,8 @@ export const Strings = {
   firstConnectionError: 'Unable to connect with master',
   connectionError: 'You have been disconnected to master',
   connectionSuccess: 'Connect with master!',
+
+  // Screen names
+  rosConnectionScreen: 'rosConnectionScreen',
+  robotControlScreen: 'rosControlScreen',
 };
