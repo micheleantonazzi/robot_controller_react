21ac5a20e186763a0e7801a783a724816b1faa7b
micheleantonazzi <micheleantonazzi@gmail.com>
2020-04-15 14:57:20 +0200

    RobotLocalizationScreen.js: added new topic which subscribes to /map

--- components/screens/RobotLocalizationScreen.js
+++ components/screens/RobotLocalizationScreen.js
@@ -1,17 +1,47 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {Button, Text, View} from 'react-native';
 import {Strings} from '../definitions/Strings';
 import RosSettingsContext from '../contexts/RosSettingsContext';
+import ROSLIB from 'roslib';
 
 const RobotLocalizationScreen = props => {
-  const rosSettings = useContext(RosSettingsContext);
+  const rosSettingsContext = useContext(RosSettingsContext);
+  const [mapListener, setMapListener] = useState(null);
 
-  // At first render, check if it is connected
+  // Create new map Listener
+  const createMapListener = () => {
+    console.log('createMapListener');
+    if (rosSettingsContext.rosSettings.ros_connector !== null) {
+
+      if (mapListener !== null) {
+        mapListener.unsubscribe();
+      }
+
+      const newMapListener = new ROSLIB.Topic({
+        ros: rosSettingsContext.rosSettings.ros_connector,
+        name: '/map',
+        messageType: 'nav_msgs/OccupancyGrid',
+      });
+
+      newMapListener.subscribe(function(message) {
+        console.log('messag');
+      });
+
+      setMapListener(newMapListener);
+    }
+  };
+
+  // At first render, check if it is connected otherwise open RosConnectionScreen
   useEffect(() => {
-    if (!rosSettings.is_connected) {
+    if (!rosSettingsContext.rosSettings.is_connected) {
       props.navigation.navigate(Strings.rosConnectionScreen);
     }
   }, []);
+
+  // Create a new mapListener only if the ros_connector changes
+  useEffect(() => {
+    createMapListener();
+  }, [rosSettingsContext.rosSettings.ros_connector]);
   return (
     <View>
       <Button title={'Modal'} onPress={() => {}} />
