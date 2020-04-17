8ed1906963674e02f3344b8ab80ad4db373bd229
micheleantonazzi <micheleantonazzi@gmail.com>
2020-04-17 12:58:43 +0200

    RobotLocalizationScreen.js: now this component renders the map inside a canvas

--- components/screens/RobotLocalizationScreen.js
+++ components/screens/RobotLocalizationScreen.js
@@ -1,19 +1,24 @@
-import React, {useContext, useEffect, useState} from 'react';
-import {Button, Text, View} from 'react-native';
+import React, {useContext, useEffect, useRef, useState} from 'react';
+import 'react-native-get-random-values';
+import {Dimensions, View} from 'react-native';
 import {Strings} from '../definitions/Strings';
 import RosSettingsContext from '../contexts/RosSettingsContext';
 import ROSLIB from 'roslib';
-import Canvas from 'react-native-canvas';
+import Canvas, {ImageData} from 'react-native-canvas';
+import resizeImageData from '../algorithms/resizeImageData';
+import {useHeaderHeight} from '@react-navigation/stack';
 
 const RobotLocalizationScreen = props => {
   const rosSettingsContext = useContext(RosSettingsContext);
   const [mapListener, setMapListener] = useState(null);
+  const [mapMessage, setMapMessage] = useState(null);
+  const canvasRef = useRef(null);
+  const headerHeight = useHeaderHeight();
 
   // Create new map Listener
   const createMapListener = () => {
     console.log('createMapListener');
     if (rosSettingsContext.rosSettings.ros_connector !== null) {
-
       if (mapListener !== null) {
         mapListener.unsubscribe();
       }
@@ -25,7 +30,7 @@ const RobotLocalizationScreen = props => {
       });
 
       newMapListener.subscribe(function(message) {
-        console.log('messag');
+        setMapMessage(message);
       });
 
       setMapListener(newMapListener);
@@ -44,16 +49,73 @@ const RobotLocalizationScreen = props => {
     createMapListener();
   }, [rosSettingsContext.rosSettings.ros_connector]);
 
-  const handleCanvas = (canvas) => {
-    const ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'purple';
-    ctx.fillRect(0, 0, 100, 100);
+  // Rerender canvas
+  useEffect(() => {
+    const screenWidth =
+      Dimensions.get('screen').width * Dimensions.get('screen').scale;
+    const screenHeight =
+      Dimensions.get('screen').height * Dimensions.get('screen').scale;
+
+    // Canvas must be a square
+    const canvasDimension =
+      screenWidth > screenHeight
+        ? Math.floor(screenHeight)
+        : Math.floor(screenWidth);
+
+    canvasRef.current.width = canvasDimension;
+    canvasRef.current.height = canvasDimension;
+    const context = canvasRef.current.getContext('2d');
+    context.clearRect(0, 0, canvasDimension, canvasDimension);
+    if (mapMessage !== null) {
+      const a = [];
+      for (let i = 0; i < mapMessage.info.width * mapMessage.info.height; ++i) {
+        const occupancyGridValue = mapMessage.data[i];
+        const pixelLuminance =
+          occupancyGridValue < 0
+            ? 205
+            : (255 * (100 - occupancyGridValue)) / 100;
+        a.push(pixelLuminance);
+        a.push(pixelLuminance);
+        a.push(pixelLuminance);
+        a.push(255);
+      }
+      const imgSrc = {
+        data: a,
+        width: mapMessage.info.width,
+        height: mapMessage.info.height,
+      };
+      const imgDst = resizeImageData(
+        imgSrc,
+        canvasDimension,
+        canvasDimension,
+        'nearest-neighbor',
+      );
+      const imageData = new ImageData(
+        canvasRef.current,
+        imgDst.data,
+        imgDst.width,
+        imgDst.height,
+      );
+
+      context.putImageData(imageData, 0, 0);
+    }
+  }, [mapMessage]);
+
+  const getViewUpHeight = () => {
+    const screenHeight = Dimensions.get('window').height;
+    const screenWidth = Dimensions.get('window').width;
+
+    if (screenHeight <= screenWidth) {
+      return screenHeight;
+    }
+
+    return (screenHeight - headerHeight - screenWidth) / 2;
   };
 
   return (
-    <View style={{flex:1}}>
-    <Canvas ref={handleCanvas}/>
-
+    <View style={{flex: 1}}>
+      <View style={{height: getViewUpHeight()}} />
+      <Canvas ref={canvasRef} />
     </View>
   );
 };
