e25a8ae0ea4cdd9c9523fac56ca7b0984cd40500
micheleantonazzi <micheleantonazzi@gmail.com>
2020-04-23 17:28:53 +0200

    StreamingCameraScreen.js: added webViewV to visualize the page which stream video

--- components/screens/StreamingCameraScreen.js
+++ components/screens/StreamingCameraScreen.js
@@ -1,10 +1,44 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {View, Dimensions} from 'react-native';
+import RosSettingsContext from '../contexts/RosSettingsContext';
+import ROSLIB from 'roslib';
+import WebView from 'react-native-webview';
 
 const StreamingCameraScreen = props => {
-  return (
-    <View>
+  const rosSettingsContext = useContext(RosSettingsContext);
+  const [webViewDimension, setWebViewDimension] = useState(
+    Dimensions.get('window'),
+  );
 
+  Dimensions.addEventListener('change', () => {
+    const screenWidth = Dimensions.get('screen').width;
+    const screenHeight = Dimensions.get('screen').height;
+
+    if (screenHeight >= screenWidth) {
+      setWebViewDimension({width: screenWidth, height: screenHeight});
+    } else {
+      setWebViewDimension({width: screenHeight + 20, height: screenHeight});
+    }
+  });
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'black',
+      }}>
+      <WebView
+        style={{
+          width: webViewDimension.width,
+          height: webViewDimension.height,
+        }}
+        source={{
+          uri: 'http://192.168.1.81:8089/stream?topic=/usb_cam/image_raw',
+        }}
+        scrollEnabled={false}
+      />
     </View>
   );
 };
