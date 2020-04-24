9a470bbc4bb29014fbb0054b2d3a2b572d7f3a0f
micheleantonazzi <micheleantonazzi@gmail.com>
2020-04-24 14:50:37 +0200

    StreamingCameraScreen.js: trying to resolve error page of web view

--- components/screens/StreamingCameraScreen.js
+++ components/screens/StreamingCameraScreen.js
@@ -1,25 +1,57 @@
-import React, {useContext, useState} from 'react';
-import {View, Dimensions} from 'react-native';
-import RosSettingsContext from '../contexts/RosSettingsContext';
-import ROSLIB from 'roslib';
+import React, {useRef, useState, useEffect} from 'react';
+import {View, Dimensions, Text, TouchableOpacity} from 'react-native';
 import WebView from 'react-native-webview';
+import {useFocusEffect} from '@react-navigation/native';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faRedo} from '@fortawesome/free-solid-svg-icons';
+import SimpleToast from 'react-native-simple-toast';
 
 const StreamingCameraScreen = props => {
-  const rosSettingsContext = useContext(RosSettingsContext);
+  const webViewRef = useRef();
+
+  // Create the uri for the web view
+  const createUri = () => {
+    return {uri: 'http://192.168.1.81:8089/stream?topic=/usb_cam/image_raw'};
+  };
   const [webViewDimension, setWebViewDimension] = useState(
     Dimensions.get('window'),
   );
+  const [webViewUri, setWebViewUri] = useState(createUri());
+  const [displayWebView, setDisplayWebView] = useState(true);
+
+  const calculateWebViewDimension = showWebView => {
+    console.log('calculate webview dim ' + showWebView);
+    if (showWebView === false) {
+      setWebViewDimension({width: 0, height: 0});
+    } else {
+      const screenWidth = Dimensions.get('screen').width;
+      const screenHeight = Dimensions.get('screen').height;
+
+      if (screenHeight >= screenWidth) {
+        setWebViewDimension({width: screenWidth, height: screenHeight});
+      } else {
+        setWebViewDimension({width: screenHeight + 20, height: screenHeight});
+      }
+    }
+  };
 
   Dimensions.addEventListener('change', () => {
-    const screenWidth = Dimensions.get('screen').width;
-    const screenHeight = Dimensions.get('screen').height;
-
-    if (screenHeight >= screenWidth) {
-      setWebViewDimension({width: screenWidth, height: screenHeight});
-    } else {
-      setWebViewDimension({width: screenHeight + 20, height: screenHeight});
-    }
+    calculateWebViewDimension(displayWebView);
   });
+
+  useEffect(() => {
+    props.navigation.addListener('focus', () => {
+      calculateWebViewDimension(false);
+      SimpleToast.show('Reload camera stream');
+      setWebViewUri({
+        html: '<body style="background-color: black"></body>',
+      });
+
+      setTimeout(() => {
+        setWebViewUri(createUri());
+      }, 1000);
+    });
+  }, [props.navigation]);
 
   return (
     <View
@@ -30,15 +62,44 @@ const StreamingCameraScreen = props => {
         backgroundColor: 'black',
       }}>
       <WebView
+        ref={webViewRef}
         style={{
           width: webViewDimension.width,
           height: webViewDimension.height,
         }}
-        source={{
-          uri: 'http://192.168.1.81:8089/stream?topic=/usb_cam/image_raw',
-        }}
+        source={webViewUri}
         scrollEnabled={false}
+        cacheEnabled={false}
+        onError={() => calculateWebViewDimension(false)}
+        onLoad={() => {
+          calculateWebViewDimension(true);
+        }}
       />
+      <View
+        style={{
+          position: 'absolute',
+          right: 20,
+          bottom: 20,
+          backgroundColor: '#42a5f6',
+          padding: 12,
+          borderRadius: 50,
+        }}>
+        <TouchableOpacity
+          onPress={() => {
+            calculateWebViewDimension(false);
+            SimpleToast.show('Reload camera stream');
+            setWebViewUri({
+              html: '<body style="background-color: black"></body>',
+            });
+
+            setTimeout(() => {
+              setWebViewUri(createUri());
+            }, 10);
+          }}
+          activeOpacity={0.7}>
+          <FontAwesomeIcon icon={faRedo} size={25} color={'white'} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
